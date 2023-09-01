6ed2dccb6c3d1faeba1cce99981bbb4e5b65c842
Иван Чамкин <chamkinivan39@gmail.com>
2023-09-01 13:17:01 +0300

    feat: add output of file characteristics

--- src/gui/slots/open_file.c
+++ src/gui/slots/open_file.c
@@ -1,6 +1,6 @@
 #include "../../s21_3dviewer.h"
 
-static gchar * get_filename_position(gchar *file_path) {
+static gchar *get_filename_position(gchar *file_path) {
     gchar *filename = file_path;
     for (; *file_path != '\0'; ++file_path) {
         if (*file_path == '/') {
@@ -10,21 +10,39 @@ static gchar * get_filename_position(gchar *file_path) {
     return ++filename;
 }
 
+static gchar *get_file_characteristics(obj_data_t *obj_data) {
+    gchar *characteristics = NULL;
+    int len = 0;
+    for (size_t i = obj_data->count_points; i > 0; i /= 10, len++);
+    for (size_t i = obj_data->count_surfaces; i > 0 ; i /=10, len++);
+
+    characteristics = calloc(20 + len, sizeof(char));
+    sprintf(characteristics, "Points:%d  Surfaces:%d", obj_data->count_points, obj_data->count_surfaces);
+    return characteristics;
+}
+
 void open_file(GtkWidget *chooser, GPtrArray *data) {
     GtkBuilder *builder = data->pdata[0];
     obj_data_t *obj_data = data->pdata[1];
 
     GtkLabel *file_name_label = (GtkLabel *)gtk_builder_get_object(builder, "File_name_label");
+    GtkLabel *file_characteristics_label = (GtkLabel *)gtk_builder_get_object(builder, "File_characteristics_label");
     GtkWidget *control_visibility_grid = (GtkWidget *) gtk_builder_get_object(builder, "Control_visibility_grid");
     GtkWidget *additional_settings_grid = (GtkWidget *) gtk_builder_get_object(builder, "Additional_settings_grid");
     GtkImage *image = (GtkImage *)gtk_builder_get_object(builder, "Model_image");
 
     free_obj_data(obj_data);
     char *file_path = gtk_file_chooser_get_filename(GTK_FILE_CHOOSER(chooser));
+    char *characteristics = NULL;
     if (parse_file(file_path, obj_data) == OK) {
         gtk_widget_set_sensitive(control_visibility_grid, TRUE);
         gtk_widget_set_sensitive(additional_settings_grid, TRUE);
+
         gtk_label_set_label(file_name_label, get_filename_position(file_path));
+        characteristics = get_file_characteristics(obj_data);
+        gtk_label_set_label(file_characteristics_label, characteristics);
+
+        free(characteristics);
         render(image, obj_data);
     } else {
         gtk_widget_set_sensitive(control_visibility_grid, FALSE);
